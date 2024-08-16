30746f8e692acb23522076375763e341d4ece480
AcronicalYT <acronicalbusiness@gmail.com>
2024-08-16 22:43:11 +0100

    remove unused methods

--- src/main/java/dev/acronical/yrrahbirthdayevent/events/impl/Runnables.java
+++ src/main/java/dev/acronical/yrrahbirthdayevent/events/impl/Runnables.java
@@ -7,22 +7,20 @@ import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
 
-public class Runnables extends BukkitRunnable {
+import java.util.Objects;
+
+public class Runnables {
 
     public static void spawnEgg() {
         if (Bukkit.getServer().getOnlinePlayers().isEmpty()) return;
-        World world = Bukkit.getServer().getOnlinePlayers().stream().findFirst().orElse(null).getWorld();
+        World world = Objects.requireNonNull(Bukkit.getServer().getOnlinePlayers().stream().findFirst().orElse(null)).getWorld();
         int mobCount = (int) world.getEntities().stream().filter(entity -> entity.getType() == EntityType.CHICKEN).count();
-        Entity chicken = world.getLivingEntities().stream().filter(livingEntity -> livingEntity.getType() == EntityType.CHICKEN).findAny().orElse(null);
-        if (chicken == null) return;
-        Location chickenLocation = chicken.getLocation();
-        world.dropItem(chickenLocation, new ItemStack(Material.EGG));
-    }
-
-    @Override
-    public void run() {
-        spawnEgg();
+        for (int i = 0; i < mobCount; i++) {
+            Entity chicken = world.getLivingEntities().stream().filter(livingEntity -> livingEntity.getType() == EntityType.CHICKEN).findAny().orElse(null);
+            if (chicken == null) return;
+            Location chickenLocation = chicken.getLocation();
+            world.dropItem(chickenLocation, new ItemStack(Material.EGG));
+        }
     }
 }
